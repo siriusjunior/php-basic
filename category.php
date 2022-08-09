255ef80dc209acdedba18fb05a730643a459fafc
siriusjunior <topdownreading@gmail.com>
2022-08-09 10:59:39 +0900

    [Add]カテゴリーの編集機能

--- category.php
+++ category.php
@@ -2,6 +2,27 @@
 include 'lib/secure.php';
 include 'lib/connect.php';
 include 'lib/queryCategory.php';
+
+$queryCategory = new QueryCategory();
+$formCategory = null;
+
+// カテゴリー新規作成
+if (!empty($_POST['action']) && $_POST['action'] == 'add' && !empty($_POST['name'])) {
+  $category = new Category();
+  $category->setName($_POST['name']);
+  $category->save();
+} else if (!empty($_GET['action']) && $_GET['action'] == 'edit' && !empty($_GET['id'])) {
+  // フォームに編集カテゴリーをセット
+  $formCategory = $queryCategory->find($_GET['id']);
+} else if (!empty($_POST['action']) && $_POST['action'] == 'edit' && !empty($_POST['id']) && !empty($_POST['name'])) {
+  $category = $queryCategory->find($_POST['id']);
+  if ($category) {
+    $category->setName($_POST['name']);
+    $category->save();
+  }
+}
+
+$categories =  $queryCategory->findAll();
 ?>
 
 <!doctype html>
@@ -46,6 +67,20 @@ include 'lib/queryCategory.php';
     <div class="row">
       <div class="col-md-12">
         <h1>カテゴリー</h1>
+        <?php if ($formCategory) : ?>
+          <h2>編集</h2>
+          <form action="category.php" method="post" class="row">
+            <input type="hidden" name="action" value="edit">
+            <input type="hidden" name="id" value="<?php echo $formCategory->getId() ?>">
+            <div class="col-md-6">
+              <input type="text" name="name" value="<?php echo $formCategory->getName() ?>" class="form-control">
+            </div>
+            <div class="col-md-6">
+              <button type="submit" class="btn btn-primary">編集する</button>
+            </div>
+          </form>
+          <hr>
+        <?php endif ?>
         <h2>新規追加</h2>
         <form action="category.php" method="post" class="row">
           <input type="hidden" name="action" value="add">
@@ -56,6 +91,29 @@ include 'lib/queryCategory.php';
             <button type="submit" class="btn btn-primary">追加する</button>
           </div>
         </form>
+        <hr>
+        <?php if ($categories) : ?>
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>カテゴリー名</th>
+                <th>編集</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($categories as $c) : ?>
+                <tr>
+                  <td><?php echo $c->getId(); ?></td>
+                  <td><?php echo $c->getName(); ?></td>
+                  <td><a href="category.php?action=edit&id=<?php echo $c->getId(); ?>" class="btn btn-success">編集</a></td>
+                </tr>
+              <?php endforeach ?>
+            </tbody>
+          </table>
+        <?php else : ?>
+          <div class="alert alert-info">カテゴリーはまだ登録されていません。</div>
+        <?php endif; ?>
       </div>
       <!-- ./col-md-12 -->
     </div>
--- lib/queryCategory.php
+++ lib/queryCategory.php
@@ -12,12 +12,66 @@ class QueryCategory extends connect
   {
     $this->category = $category;
   }
+
+  public function save()
+  {
+    $id = $this->category->getId();
+    $name = $this->category->getName();
+    if ($id) {
+      // IDがあれば上書き
+      $stmt = $this->dbh->prepare("UPDATE categories SET name=:name WHERE id=:id");
+      $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+      $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    } else {
+      // IDがなければ新規作成
+      $stmt = $this->dbh->prepare("INSERT INTO categories(name) VALUES(:name)");
+      $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    }
+    $stmt->execute();
+  }
+
+  public function find($id)
+  {
+    $stmt = $this->dbh->prepare("SELECT * FROM categories WHERE id=:id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
+      $category = new Category();
+      $category->setId($result['id']);
+      $category->setName($result['name']);
+      return $category;
+    }
+    return null;
+  }
+
+  public function findAll()
+  {
+    $stmt = $this->dbh->prepare("SELECT * FROM categories");
+    $stmt->execute();
+    $categories = array();
+    foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $result) {
+      $category = new Category();
+      $category->setId($result['id']);
+      $category->setName($result['name']);
+      $categories[$result['id']] = $category;
+    }
+    return $categories;
+  }
 }
+
 
 class Category
 {
   private $id = null;
   private $name = null;
+
+  public function save()
+  {
+    $queryCategory = new QueryCategory();
+    $queryCategory->setCategory($this);
+    $queryCategory->save();
+  }
 
   public function getId()
   {
@@ -29,7 +83,7 @@ class Category
     return $this->name;
   }
 
-  public function setid($id)
+  public function setId($id)
   {
     $this->id = $id;
   }
